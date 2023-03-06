916966f54b526f6d1d1e1d97595083181decbccf
Aphiwat Chawilai <wat.chawilai@gmail.com>
2023-03-06 16:30:49 +0700

    add solve function

--- src/App.vue
+++ src/App.vue
@@ -190,42 +190,68 @@ function selectCell(cell, slot) {
 function generatePuzzle(board, n) {
   // Remove n numbers from the board while ensuring that the resulting
   // puzzle remains solvable
-  let count = 0;
-  let loop = 0;
-  while (count < n) {
-    const row = Math.floor(Math.random() * 9);
-    const col = Math.floor(Math.random() * 9);
 
-    loop++;
+  let generated_board = board;
+  let puzzle = false;
 
-    if (board[row][col] === 0) {
-      continue;
-    }
+  while (!puzzle) {
+    try {
+      // Start the timer
+      const start = performance.now();
 
-    const savedValue = board[row][col];
+      let count = 0;
+      let loop = 0;
+      while (count < n) {
+        const row = Math.floor(Math.random() * 9);
+        const col = Math.floor(Math.random() * 9);
 
-    board[row][col] = 0;
+        loop++;
 
-    if (countSolutions(board) === 1) {
-      count++;
-    } else {
-      board[row][col] = savedValue;
+        if (generated_board[row][col] === 0) {
+          continue;
+        }
+
+        const savedValue = generated_board[row][col];
+
+        generated_board[row][col] = 0;
+
+        if (countSolutions(generated_board) === 1) {
+          count++;
+
+          if (count >= n) puzzle = true;
+        } else {
+          generated_board[row][col] = savedValue;
+        }
+
+        // Check if the time limit has been exceeded
+        const elapsed = performance.now() - start;
+        if (elapsed > 5000) {
+          console.log(`time limit exceeded : ${elapsed}`);
+          throw new Error('Generation time limit exceeded');
+        }
+      }
+    } catch (error) {
+      if (error.message === 'Generation time limit exceeded') {
+        // Retry the generation process
+        generated_board = generateSolveableSudoku();
+        continue;
+      } else {
+        // Handle other errors
+        throw error;
+      }
     }
   }
-  return board;
-}
 
-function solveSudoku() {
-  console.log('solve');
+  return generated_board;
 }
 
 function handleKeydown(event) {
   // console.log(event)
   if (selectedCell.value && selectedCell.value.value == 0) {
-    if (['1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(event.key)) { 
-      selectedCell.value.value = parseInt(event.key)
+    if (['1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(event.key)) {
+      selectedCell.value.value = parseInt(event.key);
 
-      updateNumberTotal()
+      updateNumberTotal();
     }
     // console.log(event);
     // console.log(event.keyCode);
@@ -235,16 +261,14 @@ function handleKeydown(event) {
 }
 
 function updateNumberTotal() {
-  const obj = {}      
+  const obj = {};
   for (let i = 1; i <= 9; i++) {
-    obj[`num_${i}`] = cells.value.map(item => item.value).filter(num => num === i).length;
+    obj[`num_${i}`] = cells.value
+      .map((item) => item.value)
+      .filter((num) => num === i).length;
   }
 
-  numberTotal.value = obj
-}
-
-function totalNumber(number) {
-  // return cells.value
+  numberTotal.value = obj;
 }
 
 onMounted(() => {
@@ -253,12 +277,127 @@ onMounted(() => {
 
 let board = generateSolveableSudoku();
 
-generatePuzzle(board, 40);
+console.time('generate-puzzle');
+generatePuzzle(board, 30);
+console.timeEnd('generate-puzzle');
 
 let cells = generateSudoku(board);
 let numberTotal = ref({});
 
-updateNumberTotal()
+updateNumberTotal();
+
+///
+function solveSudoku(board) {
+  const size = 9;
+
+  function getRow(row) {
+    return board[row];
+  }
+
+  function getColumn(col) {
+    return Array.from({ length: size }, (_, i) => board[i][col]);
+  }
+
+  function getBox(row, col) {
+    const boxSize = 3;
+    const startRow = Math.floor(row / boxSize) * boxSize;
+    const startCol = Math.floor(col / boxSize) * boxSize;
+    const box = [];
+
+    for (let i = 0; i < boxSize; i++) {
+      for (let j = 0; j < boxSize; j++) {
+        box.push(board[startRow + i][startCol + j]);
+      }
+    }
+
+    return box;
+  }
+
+  function getPossibleValues(row, col) {
+    const used = new Set([
+      ...getRow(row),
+      ...getColumn(col),
+      ...getBox(row, col),
+    ]);
+    const possible = new Set([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+    for (const val of used) {
+      possible.delete(val);
+    }
+
+    return possible;
+  }
+
+  function solve() {
+    for (let row = 0; row < size; row++) {
+      for (let col = 0; col < size; col++) {
+        if (board[row][col] === 0) {
+          const possibleValues = Array.from(getPossibleValues(row, col));
+
+          for (const value of possibleValues) {
+            board[row][col] = value;
+
+            if (solve()) {
+              return true;
+            }
+
+            board[row][col] = 0;
+          }
+
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  solve();
+  return board;
+}
+///
+
+function writeAnswers(board) {
+  // loop all the original board then place the number that not 0
+  let answers = solveSudoku(board);
+
+  animateSolution(answers);
+}
+
+function animateSolution(answers) {
+  const size = 9;
+  const delay = 100;
+
+  let index = 0;
+
+  // get all value = 0 in cells and collect index, row and col position in Arr
+  let zeroCells = cells.value
+    .map((item, index) => ({ index: index, value: item }))
+    .filter((item) => item.value.value == 0);
+
+  // Shuffle the copy of the array
+  for (let i = zeroCells.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [zeroCells[i], zeroCells[j]] = [zeroCells[j], zeroCells[i]];
+  }
+
+  function fillCell() {
+    // loop shuffled arr then fill value
+    if (index >= zeroCells.length) {
+      clearInterval(interval);
+      return;
+    }
+
+    cells.value[zeroCells[index].index].value =
+      answers[zeroCells[index].value.row][zeroCells[index].value.col];
+
+      updateNumberTotal();
+
+    index++;
+  }
+
+  const interval = setInterval(fillCell, delay);
+}
 
 let selectedCell = ref({});
 </script>
@@ -280,8 +419,10 @@ let selectedCell = ref({});
             (index + 1 > 45 && index + 1 <= 54),
           'border-l': (index + 1) % 3 === 0 && (index + 1) % 9 !== 0,
           'border-t': index >= 27 && index % 9 !== 0,
-          'bg-orange-100': selectedCell && (selectedCell.col === cell.col || selectedCell.row === cell.row),
-          'text-orange-400': selectedCell && (selectedCell.value === cell.value),
+          'bg-orange-100':
+            selectedCell &&
+            (selectedCell.col === cell.col || selectedCell.row === cell.row),
+          'text-orange-400': selectedCell && selectedCell.value === cell.value,
           'bg-green-200': !cell.readonly && cell.value != 0,
           'bg-gray-100': cell.readonly,
           'bg-yellow-200': cell.selected,
@@ -292,23 +433,35 @@ let selectedCell = ref({});
       </div>
     </div>
 
-    <div class="w-full mb-10 bg-blue-100 border-2 border-black py-4 px-4 rounded-xl flex justify-around items-center">
+    <div
+      class="w-full mb-10 bg-blue-100 border-2 border-black py-4 px-4 rounded-xl flex justify-around items-center"
+    >
       <div v-for="i in 9">
-        <div 
-        class="bg-gray-400 text-white font-bold py-2 px-2 rounded-md"
-        :class="{
-          'bg-orange-500' : selectedCell && (selectedCell.value === i),
-          'bg-black' : true
-        }"
-        >{{ i }} : {{ numberTotal[`num_${i}`] }}</div>
+        <div
+          class="bg-gray-400 text-white font-bold py-2 px-2 rounded-md"
+          :class="{
+            'bg-orange-500': selectedCell && selectedCell.value === i,
+            'bg-green-600': numberTotal[`num_${i}`] == 9,
+          }"
+        >
+          {{ i }} : {{ numberTotal[`num_${i}`] }}
+        </div>
       </div>
     </div>
 
+<div class="flex space-x-2">
     <button
       class="block mx-auto px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-      @click="solveSudoku()"
+      @click="writeAnswers(board)"
     >
       Solve
     </button>
+    <button
+      class="block mx-auto px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
+      @click="generateSudoku()"
+    >
+      New Game
+    </button>
+</div>
   </div>
 </template>
